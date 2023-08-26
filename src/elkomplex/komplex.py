e2bde6a438ec8d9ba290aab629cbfea0c14d6524
EL KHADIRI Yassine <yassineelkhadiri@gmail.com>
2023-08-26 16:56:13 +0200

    Improve constructor and formatin method docstrings

--- src/elkomplex/komplex.py
+++ src/elkomplex/komplex.py
@@ -14,8 +14,7 @@ from math import sqrt, cos, sin, atan2
 
 
 class Komplex:
-    """
-    Base Komplex class
+    """Base Komplex class
 
     The default constructor `Komplex()` outputs the null complex number 0.
     To build complex numbers out of cartesian or polar coordinates,
@@ -27,6 +26,7 @@ class Komplex:
     """
 
     def __init__(self):
+        """Creates the null complex number 0."""
         self._re = 0
         self._im = 0
         self._r = 0
@@ -34,6 +34,12 @@ class Komplex:
 
     @classmethod
     def from_cartesian(cls, re: float, im: float) -> 'Komplex':
+        """Creates a complex number from its real and imaginary parts
+
+        Args:
+            re (float): Real part.
+            im (float): Imaginary part.
+        """
         c = cls()
         c._re = re
         c._im = im
@@ -43,6 +49,12 @@ class Komplex:
 
     @classmethod
     def from_polar(cls, r: float, th: float) -> 'Komplex':
+        """Creates a complex number from its polar coordiates (r, theta)
+
+        Args:
+            r (float): Module.
+            th (float): Argument.
+        """
         c = cls()
         c._re = r * cos(th)
         c._im = r * sin(th)
@@ -55,13 +67,31 @@ class Komplex:
                 + f"r={self._r:0.2f}, th={self._th:0.2f})")
 
     def __str__(self) -> str:
-        """
-        Default string formating shows the complex number
-        in its a + ib cartesian form.
+        """Default string format for a Komplex number.
+
+        Shows the complex number in its a + ib cartesian form.
         """
         return str(self).__format__("0.2f")
 
     def __format__(self, spec: str) -> str:
+        """Adds str formating options `c` and `p` for Komplex numbers.
+
+        Since complex numbers can be represented by either a cartesian or polar
+        form, this method add options for string formating to choose between
+        the two.
+
+        Args:
+            spec (str):
+                Usual string formating spec.
+                The complex values (real, imaginary, module and argument)
+                shown will conform to this spec. Additionaly if `c` or `p`
+                is added at the end of the spec, representation will be
+                respectively cartesian or polar.
+
+        Example:
+            `2.0fc` will represent complex numbers with real and imaginary
+            parts with the `2.0f` spec in the cartesian format : `2.0 + 3.0 i`
+        """
         if spec.endswith('p'):
             return f"{self._r:{spec[:-1]}} e ^ (i {self._th:{spec[:-1]}})"
         if spec.endswith('c'):
