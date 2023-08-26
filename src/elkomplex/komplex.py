04cda58fe6ecec05901c7a20c25391a5b26827e1
EL KHADIRI Yassine <yassineelkhadiri@gmail.com>
2023-08-26 16:13:58 +0200

    Add str and repr formating for Komplex class

--- src/elkomplex/komplex.py
+++ src/elkomplex/komplex.py
@@ -50,3 +50,21 @@ class Komplex:
         c._th = th
         return c
 
+    def __repr__(self) -> str:
+        return (f"Komplex(re={self._re:0.2f}, im={self._im:0.2f}, "
+                + f"r={self._r:0.2f}, th={self._th:0.2f})")
+
+    def __str__(self) -> str:
+        """
+        Default string formating shows the complex number
+        in its a + ib cartesian form.
+        """
+        return str(self).__format__("0.2f")
+
+    def __format__(self, spec: str) -> str:
+        if spec.endswith('p'):
+            return f"{self._r:{spec[:-1]}} e ^ (i {self._th:{spec[:-1]}})"
+        if spec.endswith('c'):
+            return (f"{self._re:{spec[:-1]}} "
+                    + f"{'+-'[self._im<0]} {abs(self._im):{spec[:-1]}} i")
+        return f"{self._re:{spec}} {'+-'[self._im<0]} {abs(self._im):{spec}} i"
