476c6fa9560c5294c5a8194f4bf7be20429d59a5
EL KHADIRI Yassine <yassineelkhadiri@gmail.com>
2023-08-26 19:45:19 +0200

    Improve arithmetic methods docstrings

--- src/elkomplex/komplex.py
+++ src/elkomplex/komplex.py
@@ -103,6 +103,7 @@ class Komplex:
         return f"{self._re:{spec}} {'+-'[self._im<0]} {abs(self._im):{spec}} i"
 
     def __eq__(self, other: int | float | Komplex) -> bool:
+        """Check if real and imaginary parts are close enough to be equal"""
         match other:
             case int() | float():
                 return (isclose(self._re, other, abs_tol=EQTOL)
@@ -114,6 +115,7 @@ class Komplex:
                 return NotImplemented
 
     def __add__(self, other: int | float | Komplex) -> Komplex:
+        """Piecewise addition between real and imaginary parts"""
         match other:
             case int() | float():
                 return Komplex.from_cartesian(other + self._re, self._im)
@@ -124,9 +126,11 @@ class Komplex:
                 return NotImplemented
 
     def __radd__(self, other: int | float | Komplex) -> Komplex:
+        """Addition is symmetric"""
         return self.__add__(other)
 
     def __sub__(self, other: int | float | Komplex) -> Komplex:
+        """Piecewise substraction between real and imaginary parts"""
         match other:
             case int() | float():
                 return Komplex.from_cartesian(self._re - other, self._im)
@@ -137,6 +141,7 @@ class Komplex:
                 return NotImplemented
 
     def __rsub__(self, other: int | float | Komplex) -> Komplex:
+        """Piecewise substraction between real and imaginary parts"""
         match other:
             case int() | float():
                 return Komplex.from_cartesian(other - self._re, -self._im)
@@ -147,6 +152,14 @@ class Komplex:
                 return NotImplemented
 
     def __mul__(self, other: int | float | Komplex) -> Komplex:
+        """Simple implementation using polar coordinates
+
+        z1 = rho1 e^(i th1)
+
+        z2 = rho2 e^(i th2)
+
+        z1 * z2 = (rho1 * rho2) e^(i (th1 + th2) % 2pi)
+        """
         match other:
             case int() | float():
                 return Komplex.from_polar(other * self._r, self._th)
@@ -157,4 +170,7 @@ class Komplex:
                 return NotImplemented
 
     def __rmul__(self, other: int | float | Komplex) -> Komplex:
+        """Multiplication is symmetric.
+
+        See :py:meth:`__mul__` for more details."""
         return self.__mul__(other)
